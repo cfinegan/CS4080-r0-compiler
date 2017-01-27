56afcf601fd589953ce5b50db001cb0113eed5cb
Conor Finegan <conor_finegan@student.uml.edu>
2017-01-27 13:36:48 -0500

    Added new test case.

--- test/uniquify-test.rkt
+++ test/uniquify-test.rkt
@@ -4,5 +4,11 @@
 (require "../uniquify.rkt")
 
 (test-begin
+ 
  (check-equal? (uniquify '(let ((x 10) (y 6)) (+ (let ((z 1) (y 4)) y) x)))
-               '(let ((x_0 10) (y_0 6)) (+ (let ((z_1 1) (y_1 4)) y_1) x_0))))
+               '(let ((x_0 10) (y_0 6)) (+ (let ((z_1 1) (y_1 4)) y_1) x_0)))
+
+ (check-equal? (uniquify '((let ((x 0) (y 2)) (+ x y)) (+ e f)))
+               '((let ((x_0 0) (y_0 2)) (+ x_0 y_0)) (+ e f)))
+
+ )
